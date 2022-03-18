75ae0332d17baede67d4e47b1292abda6f5e780d
Limstar12 <phji0326@gmail.com>
2022-03-18 11:11:04 +0900

    노드라우팅완료

--- phj.js
+++ phj.js
@@ -1,9 +1,17 @@
 var express = require('express')
 var app = express();
 
+var postsend = require('./routes/postsend')
+var getsend = require('./routes/getsend')
+var sqlsend = require('./routes/sqlsend')
+
 app.get('/', (req,res,next) => {
     res.send('첫페이지 출력된다')
 })
+
+app.use('/getsend', getsend)
+app.use('/postsend', postsend)
+app.use('/sqlsend', sqlsend)
 
 app.listen(8080, () => {
     console.log('콘솔창을 확인해봐 서버구동완료')
