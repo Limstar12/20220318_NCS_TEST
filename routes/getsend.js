e19e18c21c1f099cf2331eca0a84844d75aa2938
Limstar12 <phji0326@gmail.com>
2022-03-18 12:26:12 +0900

    리액트 db 연동 준비 완료

--- routes/getsend.js
+++ routes/getsend.js
@@ -1,8 +1,24 @@
+const { Router } = require('express');
 var express = require('express')
+var mysql = require('mysql')
+var dbconfig = require('../db/dbconfig')
 var routes = express.Router();
 
 routes.get('/', (req,res,next) => {
     res.send('눈누난나')
+})
+
+routes.get('/getjson', (req,res,next) => {
+    res.send({'url':'ddddddddddd',
+              '바보냐' : '멍청이냐'  })
+})
+
+routes.get('/awssql', (req, res, next) => {
+    connection.query('SELECT * FROM reactinterview.interview', (error, result) =>{
+        if(error) throw error;
+        console.log('해당스키마안의 테이블내용:', result)
+        res.send(result)
+        })
 })
 
 module.exports=routes;
